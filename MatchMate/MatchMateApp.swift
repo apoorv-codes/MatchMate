7763f75a325d70df68ce012deda14048e91af7f4
Apoorv Verma <veramsunny224@gmail.com>
2024-09-05 21:31:49 +0530

    Initial Commit

---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Apoorv Verma on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatchMateTests/MatchMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateTests.swift
+//  MatchMateTests
+//
+//  Created by Apoorv Verma on 9/5/24.
+//
+
+import Testing
+@testable import MatchMate
+
+struct MatchMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
